798013ddff7cc944f66a727a1542a6850dbe027d
Thang Nguyen <thangnn86@gmail.com>
2018-10-27 12:55:03 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
@@ -11,14 +12,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button variant="contained" color="primary">
+      Hello World
+    </Button>
         </header>
       </div>
     );
